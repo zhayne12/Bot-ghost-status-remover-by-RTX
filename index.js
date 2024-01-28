e31809eee69476cc3502dc4f66693174e8d6b1e4
zhayne12 <156608502+zhayne12@users.noreply.github.com>
2024-01-27 22:39:18 -0600

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["👑 I am RTX","Spotify","Youtube","SUBSCRIBE❤️","THANKS 🌇"];
+const statusMessages = ["moods is daddy"];
 
 
 let currentIndex = 0;
